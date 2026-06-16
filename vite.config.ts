d521519e6ca1cc59a684d15b527a70d5092f260e
Miguel Quiroz <contato@codestrm.com.br>
2026-06-16 15:54:15 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  plugins: [
+    react(), 
+    process.env.NODE_ENV === "development" && componentTagger()
+  ].filter(Boolean),
   server: {
     host: "::",
     port: 8080,
@@ -12,10 +16,9 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
